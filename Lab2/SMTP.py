ae102eda993b3642c80b5a90ef746967eae99f05
utkvrma <you@example.com>
2025-09-09 14:59:36 +0530

    Switched to ethereal instead of Mail Trap

--- Lab2/SMTP.py
+++ Lab2/SMTP.py
@@ -1,25 +1,28 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-def send_email():
-    sender = "hello@demomailtrap.co"
-    receiver = "utkarshiitp27@gmail.com"
-    username = "api"
-    password = "password"   
+SMTP_SERVER = "smtp.ethereal.email"
+SMTP_PORT = 587  
+USERNAME = "samantha68@ethereal.email"
+PASSWORD = "tx5jY9hb7VRPhq3kqs"
 
-    msg = MIMEText("Hello Utkarsh, I am Mailtrap.")
-    msg["Subject"] = "CN Lab 2 - Test"
-    msg["From"] = sender
-    msg["To"] = receiver
+msg = MIMEMultipart()
+msg["From"] = USERNAME
+msg["To"] = "utkarshiitp27@gmail.com"
+msg["Subject"] = "Hello from Python + Ethereal"
 
-    try:
-        s = smtplib.SMTP("live.smtp.mailtrap.io", 587)
-        s.starttls()
-        s.login(username, password)
-        s.sendmail(sender, receiver, msg.as_string())
-        s.quit()
-        print("Email sent successfully.")
-    except Exception as e:
-        print("Error:", e)
+body = "This is a test email sent via smtplib and Ethereal from Samantha Rosenbaum!"
+msg.attach(MIMEText(body, "plain"))
 
-send_email()
+try:
+    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    server.starttls() 
+    server.login(USERNAME, PASSWORD)
+
+    server.sendmail(msg["From"], msg["To"], msg.as_string())
+    print("Email sent successfully!")
+
+    server.quit()
+except Exception as e:
+    print("Error:", e)
